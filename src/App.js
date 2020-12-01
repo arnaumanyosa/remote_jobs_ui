aba0115951bb432ea076001a9da9723c3b0455aa
arnaumanyosa <arnau.manyosa@protonmail.com>
2020-12-01 19:12:35 +0100

    basic data fetching with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState( [] );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://remote-cat-api.herokuapp.com/jobs'
+      );
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Remote Jobs</h1>
       </header>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <a href="{item.url">{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
