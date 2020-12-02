01dd42775cf206572c39a09d4f780e4cdd214007
arnaumanyosa <arnau.manyosa@protonmail.com>
2020-12-02 10:53:59 +0100

    added simple error message

--- src/App.js
+++ src/App.js
@@ -9,20 +9,26 @@ function App() {
   const [query, setQuery] = useState('');
   const [search, setSearch] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsError(false);
       setIsLoading(true);
-      const result = await axios(
-        'https://remote-cat-api.herokuapp.com/jobs'
-      );
 
-      // Service atm does not include a search option
-      // so just filter all the results
-      const filteredResult = result.data
-        .filter(text => text.title.toLowerCase().includes(search.toLowerCase()));
+      try {
+        const result = await axios('https://remote-cat-api.herokuapp.com/jobs');
 
-      setData(filteredResult);
+        // Service atm does not include a search option
+        // so just filter all the results
+        const filteredResult = result.data
+          .filter(text => text.title.toLowerCase().includes(search.toLowerCase()));
+
+        setData(filteredResult);
+      } catch(error) {
+        setIsError(true);
+      }
+
       setIsLoading(false);
     };
 
@@ -42,17 +48,20 @@ function App() {
       <button type="button" onClick={() => setSearch(query)}>
         Search
       </button>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul>
-            {data.map(item => (
-              <li key={item.id}>
-                <a href="{item.url">{item.title}</a>
-              </li>
-            ))}
-          </ul>
-        )}
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>
+              <a href="{item.url">{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
